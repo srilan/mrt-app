c15be3407a1d2a901a4c19e2c57bc95ac6966fc5
srilan <devd@Devs-MacBook-Pro.local>
2023-12-30 05:33:07 +0800

    added mock data

---
+++ src/services/stations.ts
@@ -0,0 +1,17 @@
+export const stations = [
+  {
+    name: 'Cubao',
+    id: 1,
+    shortName: 'cubao',
+  },
+  {
+    name: 'Magallanes',
+    id: 2,
+    shortName: 'magallanes',
+  },
+  {
+    name: 'Ayala',
+    id: 3,
+    shortName: 'ayala',
+  }
+]
